6912322f08cacf224ed076cd17571c5e2165e40f
Özlem Ateş <ozlemxatess@outlook.com>
2023-05-15 16:53:26 +0300

    new challenge has added

---
+++ Day 3-Arrays/arrays.js
@@ -0,0 +1,39 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    nums = new Set(nums)
+    nums=Array.from(nums);
+    nums=nums.sort(com);
+    return nums [1];  
+}
+
+function com(a,b) {
+    return b-a;
+}
