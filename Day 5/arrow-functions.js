8a63c041e1bf69e10ff402b53f7e0b9adf2b0458
Özlem Ateş <ozlemxatess@outlook.com>
2023-05-18 12:44:57 +0300

    new challenge has added

---
+++ Day 5/arrow-functions.js
@@ -0,0 +1,41 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// function modifyArray(nums) {
+//     const multiplyByTwoIfEven = (element) => {
+//         if (element % 2 === 0) {
+//             return element * 2;
+//         } else {
+//             return element * 3;
+//           }
+//     }
+// }
+
+// function()
+
+const modifyArray = (element) => (element %2 ===0) ? (element *2): (element *3)function modifyArray(nums) {
+    const modifiedArray = nums.map(element => (element % 2 === 0) ? (element * 2) : (element * 3));
+    
+    return modifiedArray;
+}
