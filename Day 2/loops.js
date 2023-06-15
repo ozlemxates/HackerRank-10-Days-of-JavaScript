55af914fc32a1cb13f022266aaa37892e45ddad1
Özlem Ateş <ozlemxatess@outlook.com>
2023-06-15 21:57:04 +0300

    Correct a mistake in challenge:loops

--- Day 2/loops.js
+++ Day 2/loops.js
@@ -1,5 +1,45 @@
-let message = "Hello, world!";
-let number = 42;
+'use strict';
 
-console.log(message);
-console.log("The answer is:", number);
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function vowelsAndConsonants(s) {
+    for (let i = 0; i < s.length; i++) {
+        const letter = s[i];
+        if (letter === 'a' || letter === 'e' || letter === 'i' || letter === 'o') {
+            console.log(letter);
+        }
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        const letter = s[i];
+        if (letter !== 'a' && letter !== 'e' && letter !== 'i' && letter !== 'o') {
+            console.log(letter);
+        }
+    }
+}
+
+function main() {
+    const s = readLine();
+    
+    vowelsAndConsonants(s);
+}
