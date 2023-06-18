250b8c40c5408780ca5d3251a2cf3e7058d91c27
Özlem Ateş <ozlemxatess@outlook.com>
2023-06-18 18:25:15 +0300

    All js documents are now have html documents

--- Day 7/script.js
+++ Day 7/script.js
@@ -1,1 +1,20 @@
+// let counter = 0
 
+// while (counter < 10) {
+//     console.log(counter)
+//     counter++
+// }
+
+const PRODUCTS = ["laptop", "bilgisayar", "cüzdan", "meyve", "sebze", "baston"]
+const NEW_PRODUCTS = PRODUCTS.filter(item =>item.length > 5)
+console.log(NEW_PRODUCTS)
+
+const USERS = [
+    {fullName: "Cansu Özden", isactive: false},
+    {fullName: "Murat Yılmaz", isactive: true},
+    {fullName: "Sezer Ateş", isactive: true},
+    {fullName: "Özlem Ateş", isactive: false}
+]
+
+const ACTIVE_USERS = USERS.filter(user=> user.isactive)
+console.log(ACTIVE_USERS)
