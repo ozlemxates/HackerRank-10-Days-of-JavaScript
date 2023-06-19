dea36af5c344fbf34027860f316774c9a0bf5fe1
Özlem Ateş <ozlemxatess@outlook.com>
2023-06-19 18:43:11 +0300

    Add solution for day 6

--- Day 6/javaScript-dates.js
+++ Day 6/javaScript-dates.js
@@ -22,14 +22,29 @@ function readLine() {
     return inputString[currentLine++];
 }
 
-// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
-function getDayName(dateString) {
-    let dayName;
-    // Write your code here
-    
-    return dayName;
-}
 
+function getDayName(dateString) {
+  const dayIndex = new Date(dateString).getDay();
+  
+  switch (dayIndex) {
+    case 0:
+      return "Sunday";
+    case 1:
+      return "Monday";
+    case 2:
+      return "Tuesday";
+    case 3:
+      return "Wednesday";
+    case 4:
+      return "Thursday";
+    case 5:
+      return "Friday";
+    case 6:
+      return "Saturday";
+    default:
+      return "Invalid Date";
+  }
+}
 
 function main() {
     const d = +(readLine());
