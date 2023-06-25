93ccce591170042ee7db2ffd8d807aadb8ead091
Özlem Ateş <ozlemxatess@outlook.com>
2023-06-25 21:52:27 +0300

    Fucktion is now better

--- Day 8/buttons-container.js
+++ Day 8/buttons-container.js
@@ -1,69 +1,23 @@
-let btn1 = 1;
-let btn2 = 2;
-let btn3 = 3;
-let btn4 = 4;
-let btn5 = 5;
-let btn6 = 6;
-let btn7 = 7;
-let btn8 = 8;
-let btn9 = 9;
+let btn1 = document.querySelector("#btn1").innerHTML;
+let btn2 = document.querySelector("#btn2").innerHTML;
+let btn3 = document.querySelector("#btn3").innerHTML;
+let btn4 = document.querySelector("#btn4").innerHTML;
+let btn5 = document.querySelector("#btn5");
+let btn6 = document.querySelector("#btn6").innerHTML;
+let btn7 = document.querySelector("#btn7").innerHTML;
+let btn8 = document.querySelector("#btn8").innerHTML;
+let btn9 = document.querySelector("#btn9").innerHTML;
+console.log(btn2);
 
-
-let buttons = document.querySelectorAll(".btn")
-let btn1DOM = document.querySelector("#btn1")
-let btn2DOM = document.querySelector("#btn2")
-let btn3DOM = document.querySelector("#btn3")
-let btn4DOM = document.querySelector("#btn4")
-let btn5DOM = document.querySelector("#btn5")
-let btn6DOM = document.querySelector("#btn6")
-let btn7DOM = document.querySelector("#btn7")
-let btn8DOM = document.querySelector("#btn8")
-let btn9DOM = document.querySelector("#btn9")
-
-btn5DOM.addEventListener("click", clickEvent)
-
-
-btn1DOM.innerHTML = btn1
-btn2DOM.innerHTML = btn2
-btn3DOM.innerHTML = btn3
-btn4DOM.innerHTML = btn4
-btn5DOM.innerHTML = btn5
-btn6DOM.innerHTML = btn6
-btn7DOM.innerHTML = btn7
-btn8DOM.innerHTML = btn8
-btn9DOM.innerHTML = btn9
-
-let clickCount = 0; 
+btn5.addEventListener("click", clickEvent);
 
 function clickEvent() {
-  if (this.id === "btn5") {
-    clickCount++; 
-    if (clickCount === 1) {
-      handleFirstClick();
-    } else if (clickCount === 2) {
-      handleSecondClick();
-    }
-  }
-}
-
-function handleFirstClick() {
-  btn1DOM.innerHTML = btn1 + 3;
-  btn2DOM.innerHTML = btn2 - 1;
-  btn3DOM.innerHTML = btn3 - 1;
-  btn4DOM.innerHTML = btn4 + 3;
-  btn6DOM.innerHTML = btn6 - 3;
-  btn7DOM.innerHTML = btn7 + 1;
-  btn8DOM.innerHTML = btn8 + 1;
-  btn9DOM.innerHTML = btn9 - 3;
-}
-
-function handleSecondClick() {
-    btn1DOM.innerHTML = btn1 + 6;
-    btn2DOM.innerHTML = btn2 + 2;
-    btn3DOM.innerHTML = btn3 - 2;
-    btn4DOM.innerHTML = btn4 + 4;
-    btn6DOM.innerHTML = btn6 - 4;
-    btn7DOM.innerHTML = btn7 + 2;
-    btn8DOM.innerHTML = btn8 - 2;
-    btn9DOM.innerHTML = btn9 - 6; 
+  document.getElementById('btn1').innerHTML = btn4;
+  document.getElementById('btn2').innerHTML = btn1;
+  document.getElementById('btn3').innerHTML = btn2;
+  document.getElementById('btn4').innerHTML = btn7;
+  document.getElementById('btn6').innerHTML = btn3;
+  document.getElementById('btn7').innerHTML = btn8;
+  document.getElementById('btn8').innerHTML = btn9;
+  document.getElementById('btn9').innerHTML = btn6;
 }
