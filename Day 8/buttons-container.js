bfdfd4f9b2858a27dfd453d3ce14fdfbc0c6c1bb
Özlem Ateş <ozlemxatess@outlook.com>
2023-08-05 16:32:56 +0300

    Remove eventlistener

--- Day 8/buttons-container.js
+++ Day 8/buttons-container.js
@@ -8,25 +8,21 @@ let btn7 = document.querySelector("#btn7");
 let btn8 = document.querySelector("#btn8");
 let btn9 = document.querySelector("#btn9");
 
-btn5.addEventListener("click", clickEvent);
 function clickEvent() {
-  if (this === btn5) {
-    let button1 = btn1.innerHTML;
-    let button2 = btn2.innerHTML;
-    let button3 = btn3.innerHTML;
-    let button4 = btn4.innerHTML;
-    let button6 = btn6.innerHTML;
-    let button7 = btn7.innerHTML;
-    let button8 = btn8.innerHTML;
-    let button9 = btn9.innerHTML;
-
-    btn1.innerHTML = button4;
-    btn2.innerHTML = button1;
-    btn3.innerHTML = button2;
-    btn4.innerHTML = button7;
-    btn6.innerHTML = button3;
-    btn7.innerHTML = button8;
-    btn8.innerHTML = button9;
-    btn9.innerHTML = button6;
-  }
+  let button1 = btn1.innerHTML;
+  let button2 = btn2.innerHTML;
+  let button3 = btn3.innerHTML;
+  let button4 = btn4.innerHTML;
+  let button6 = btn6.innerHTML;
+  let button7 = btn7.innerHTML;
+  let button8 = btn8.innerHTML;
+  let button9 = btn9.innerHTML;
+  btn1.innerHTML = button4;
+  btn2.innerHTML = button1;
+  btn3.innerHTML = button2;
+  btn4.innerHTML = button7;
+  btn6.innerHTML = button3;
+  btn7.innerHTML = button8;
+  btn8.innerHTML = button9;
+  btn9.innerHTML = button6;
 }
